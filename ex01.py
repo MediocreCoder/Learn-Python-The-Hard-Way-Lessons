92ca9579743df1a9d0b72dee5a4bc278fa9a2752
MediocreCoder <demarcoz11@gmail.com>
2013-08-26 21:50:52 -0400

    A Good First Program

--- ex01.py
+++ ex01.py
@@ -6,4 +6,4 @@ print "Yay! Printing."
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 # <-This is an ‘octothorpe’ which is also called a ‘pound’, ‘hash’, or ‘mesh’
-#Octothorpes make python ignore a line of code which is useful for commenting to yourself or other coders!
+# These octothorpes make python ignore a line of code which is useful for commenting both to yourself and to other coders!
