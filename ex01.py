2de1818e4bfbd1421584c65301f038da2d9f51d1
MediocreCoder <demarcoz11@gmail.com>
2013-08-26 21:04:33 -0400

    A Good First Program

---
+++ ex01.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# <-This is an ‘octothorpe’ which is also called a ‘pound’, ‘hash’, or ‘mesh’
+#Octothorpes make python ignore a line of code which is useful for commenting to yourself or other coders!
