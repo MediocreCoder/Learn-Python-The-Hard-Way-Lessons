c3c551c14cec7e25bd5680b43772a018b7871cdc
MediocreCoder <demarcoz11@gmail.com>
2013-08-28 23:12:43 -0400

    More Variables And Printing

---
+++ ex05.py
@@ -0,0 +1,21 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall or %d centimeters tall." % (height, height * 2.54)
+print "He's %d pounts heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
+    
+# For a list of conversion flag characters and conversion types.
+# Follow this link: http://docs.python.org/2/library/stdtypes.html#string-formatting
