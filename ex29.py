582c7dd956431f9b40523a4592c5b18dc2919f1f
MediocreCoder <demarcoz11@gmail.com>
2013-09-11 18:37:13 -0400

    What If

---
+++ ex29.py
@@ -0,0 +1,38 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+    
+if people > cats:
+    print "Not many cats! The world is saved!"
+    
+if people < dogs:
+    print "The world is drooled on!"
+
+if people > dogs:
+    print "The world is dry!"
+    
+    
+dogs += 5
+
+if people >= dogs:
+    print "People are greater than equal to dogs."
+    
+if people <= dogs:
+    print "People are less than equal to dogs."
+    
+    
+if people == dogs:
+    print "People are dogs."
+    
+"""
+IF STATEMENTS are boolean focused, True or False.
+
+How they work:
+If an offer to an "if" statement is True, 
+    the code within its indented block of code executes!
+False offer's code will not be executed.
+"""
