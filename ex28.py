abeb5677576f0567a25b9f4eee0850e2f8661542
MediocreCoder <demarcoz11@gmail.com>
2013-09-10 21:43:14 -0400

    Boolean Practice

---
+++ ex28.py
@@ -0,0 +1,109 @@
+print """
+Exercise 28: Boolean Practice
+##################################################
+Instructions:
+    Simply press enter to reveal each statement's
+                ultimate outcome... 
+                
+                True or False!
+                
+=================================================="""
+#1
+print "\n\nTrue and True"
+raw_input()
+print True and True
+
+#2
+print "\n\nFalse and True"
+raw_input()
+print False and True
+
+#3
+print "\n\n1 == 1 and 2 == 1"
+raw_input()
+print 1 == 1 and 2 == 1
+
+#4
+print '\n\n"test" == "test"'
+raw_input()
+print "test" == "test"
+
+#5
+print '\n\n1 == 1 or 2 != 1'
+raw_input()
+print 1 == 1 or 2 != 1
+
+#6
+print '\n\nTrue and 1 == 1'
+raw_input()
+print True and 1 == 1
+
+#7
+print '\n\nFalse and 0 != 0'
+raw_input()
+print False and 0 != 0
+
+#8
+print '\n\nTrue or 1 == 1'
+raw_input()
+print True or 1 == 1
+
+#9
+print '\n\n"test" == "testing"'
+raw_input()
+print "test" == "testing"
+
+#10
+print '\n\n1 != 0 and 2 == 1'
+raw_input()
+print 1 != 0 and 2 == 1
+
+#11
+print '\n\n"test" != "testing"'
+raw_input()
+print "test" != "testing"
+
+#12
+print '\n\n"test" == 1'
+raw_input()
+print "test" == 1
+
+#13
+print '\n\nnot (True and False)'
+raw_input()
+print not (True and False)
+
+#14
+print '\n\nnot (1 == 1 and 0 != 1)'
+raw_input()
+print not (1 == 1 and 0 != 1)
+
+#15
+print '\n\nnot (10 == 1 or 1000 == 1000)'
+raw_input()
+print not (10 == 1 or 1000 == 1000)
+
+#16
+print '\n\nnot (1 != 10 or 3 == 4)'
+raw_input()
+print not (1 != 10 or 3 == 4)
+
+#17
+print '\n\nnot ("testing" == "testing" and "Zed" == "Cool Guy")'
+raw_input()
+print not ("testing" == "testing" and "Zed" == "Cool Guy")
+
+#18
+print '\n\n1 == 1 and not ("testing" == 1 or 1 == 0)'
+raw_input()
+print 1 == 1 and not ("testing" == 1 or 1 == 0)
+
+#19
+print '\n\n"chunky" == "bacon" and not (3 == 4 or 3 == 3)'
+raw_input()
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+
+#20
+print '\n\n3 == 3 and not ("testing" == "testing" or "Python" == "Fun")'
+raw_input()
+print 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
